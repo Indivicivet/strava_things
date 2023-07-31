847071ca5ce5b705dc8791a6e31a5319fed8987d
indi <indi.pritchard@vivid-q.com>
2023-07-31 13:31:08 +0100

    add hard runs script with some hardcoded stuff

---
+++ hard_runs.py
@@ -0,0 +1,42 @@
+import requests
+import time
+from pathlib import Path
+from bs4 import BeautifulSoup
+
+#resp = requests.post(
+#    "https://www.strava.com/oauth/token?"
+#    "client_id=CLIENT_ID&"
+#    "client_secret=CLIENT_SECRET&"
+#    "code=CODE_FROM_THING&"
+#    "grant_type=authorization_code"
+#)
+#print(resp.json())
+
+token = (
+    Path(__file__).parents[1] / "access_tokens_2023" / "strava_token.txt"
+).read_text().strip()
+
+def get_activity(id=9535755237):
+    return requests.get(
+        f"https://www.strava.com/api/v3/activities/{id}"
+        f"?include_all_efforts=",
+        headers={"Authorization": "Bearer " + token}
+    )
+
+def get_activities(per_page=30):
+    return requests.get(
+        f"https://www.strava.com/api/v3/athlete/activities?before="
+        f"{int(time.time())}&after=964796319&per_page={per_page}",
+        headers={"Authorization": "Bearer " + token},
+    ).json()
+
+ra = get_activities(80)
+
+for l in [
+    (r["start_date"], r["average_speed"] / r["average_heartrate"])
+    for r in ra
+    if r["has_heartrate"]
+       and r["distance"] > 5000
+       and r["max_heartrate"] > 150
+]:
+    print(l[0].replace("T", " ").replace("Z", ""), ",", l[1])
