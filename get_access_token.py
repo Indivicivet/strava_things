eb785e8a75034fd7069dd6744e8b9cc5fd21c13a
indi <indivicivet@gmail.com>
2023-08-04 22:45:11 +0100

    add incredibly useful "get access token" script

---
+++ get_access_token.py
@@ -0,0 +1,27 @@
+import requests
+
+client_id = input(
+    "go to https://www.strava.com/settings/api\n"
+    "client_id=? "
+)
+client_secret = input("client_secret=? ")
+code = input(
+    "now go to (and Authorize):\n"
+    "https://www.strava.com/oauth/authorize"
+    f"?client_id={client_id}"
+    "&redirect_uri=https://localhost"
+    "&response_type=code&scope=activity:read_all"
+    "\n(copy from browser URL) code=? "
+)
+
+resp = requests.post(
+    "https://www.strava.com/oauth/token?"
+    f"client_id={client_id}&"
+    f"client_secret={client_secret}&"
+    f"code={code}&"
+    "grant_type=authorization_code"
+).json()
+print(resp)
+print()
+print("access token:", resp["access_token"])
+print("refresh token:", resp["refresh_token"])
