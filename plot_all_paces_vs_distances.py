453c368433d6f8f0cec93877ce71dfd9760eb276
indi <indivicivet@gmail.com>
2023-08-05 18:40:18 +0100

    restructure code a bit, separate out the plotting loop

--- plot_all_paces_vs_distances.py
+++ plot_all_paces_vs_distances.py
@@ -14,8 +14,9 @@ all_data = {
     for activity_id, data in json.loads(p.read_text()).items()
 }
 
+
+velocity_and_distance = {}
 for activity_id, info in all_data.items():
-    print(f"{activity_id=}")
     this_velocities = None
     this_distances = None
     for retrieved in info:
@@ -23,14 +24,15 @@ for activity_id, info in all_data.items():
             this_velocities = retrieved["data"]
         if retrieved["type"] == "distance":
             this_distances = retrieved["data"]
-    if this_distances is None:
-        print("no distances?")
-        continue
-    if this_velocities is None:
-        print("no velocities?")
-        continue
-    average_velocity = sum(this_velocities) / len(this_velocities)
-    plt.plot([0, max(this_distances)], [average_velocity] * 2)
+    if this_distances is not None and this_velocities is not None:
+        velocity_and_distance[activity_id] = {
+            "velocity": this_velocities,
+            "distance": this_distances,
+        }
+
+for _, data in velocity_and_distance.items():
+    average_velocity = sum(data["velocity"]) / len(data["velocity"])
+    plt.plot([0, max(data["distance"])], [average_velocity] * 2)
 
 plt.ylabel("velocity")
 plt.xlabel("distance")
