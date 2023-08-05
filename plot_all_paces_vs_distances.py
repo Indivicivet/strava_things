d770b98162a28ee299cca75f3ecb1cbac8b9e9d7
indi <indivicivet@gmail.com>
2023-08-05 18:34:41 +0100

    plot all paces vs distances... literally (this is a nonsense plot, shall be improved!)

--- plot_all_paces_vs_distances.py
+++ plot_all_paces_vs_distances.py
@@ -1,6 +1,8 @@
 import json
 from pathlib import Path
 
+from matplotlib import pyplot as plt
+
 MY_DATA_FOLDER = Path(__file__).parent / "my_data"
 
 all_data = {
@@ -11,5 +13,19 @@ all_data = {
 
 for activity_id, info in all_data.items():
     print(f"{activity_id=}")
-    for thing in info:
-        print(thing["type"], thing["original_size"])
+    this_velocities = None
+    this_distances = None
+    for retrieved in info:
+        if retrieved["type"] == "velocity_smooth":
+            this_velocities = retrieved["data"]
+        if retrieved["type"] == "distance":
+            this_distances = retrieved["data"]
+    if this_distances is None:
+        print("no distances?")
+        continue
+    if this_velocities is None:
+        print("no velocities?")
+        continue
+    plt.plot(this_distances, this_velocities)
+
+plt.show()
