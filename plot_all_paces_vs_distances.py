cc09a249c119857b241875cb47e545f495883256
indi <indivicivet@gmail.com>
2023-08-05 18:55:35 +0100

    veryyyy slow code that seems to *mostly* work, many kinks to iron out

--- plot_all_paces_vs_distances.py
+++ plot_all_paces_vs_distances.py
@@ -50,9 +50,36 @@ for activity_id, info in all_data.items():
             )
         )
 
-for run in runs:
-    average_velocity = sum(run.velocity) / len(run.velocity)
-    plt.plot([0, max(run.distance)], [average_velocity] * 2)
+
+#max_distance = max(run.distance[-1] for run in runs)
+PLOT_DISTANCE_INTERVAL = 100
+
+for run in runs[:3]:
+    intervals = range(0, int(run.distance[-1]), PLOT_DISTANCE_INTERVAL)
+    interval_speeds = []
+    for interval in intervals:
+        shortest_time = 9999
+        for start_i, start_d in enumerate(run.distance):
+            delta_i = None
+            try:
+                delta_i = next(
+                    i
+                    for i, d in enumerate(run.distance[start_i + 1:])
+                    if (d - start_d) > interval
+                )
+            except StopIteration:
+                break
+            else:
+                shortest_time = min(
+                    shortest_time,
+                    run.time[start_i + delta_i] - run.time[start_i],
+                )
+        if shortest_time == 0:
+            print(f"shortest_time is zero!?")
+            interval_speeds.append(0)
+        else:
+            interval_speeds.append(interval / shortest_time)
+    plt.plot(intervals, interval_speeds)
 
 plt.ylabel("velocity")
 plt.xlabel("distance")
