28ad10cbaef46bb5e1bc03f5e72b5b6a5816bf70
indi <indivicivet@gmail.com>
2023-08-24 14:31:47 +0100

    attempt to colour by HR but can't seem to pass through array of values??

--- plot_all_paces_vs_distances.py
+++ plot_all_paces_vs_distances.py
@@ -3,7 +3,9 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional
 
+import matplotlib
 from matplotlib import pyplot as plt
+import numpy as np
 import seaborn
 from tqdm import tqdm
 
@@ -70,15 +72,27 @@ for activity_id, info in all_data.items():
 #max_distance = max(run.distance[-1] for run in runs)
 PLOT_DISTANCE_INTERVAL = 100
 
-for run in tqdm(runs):
+
+def hr_to_01(hr, max_hr=210, min_hr=100):
+    return np.clip(
+        (max_hr - np.array(hr)) / (max_hr - min_hr),
+        0, 1
+    )
+
+
+color_map = matplotlib.cm.get_cmap("RdYlBu")
+
+for run in tqdm(runs[:5]):
     intervals = range(
         PLOT_DISTANCE_INTERVAL * 2,
         int(run.distance[-1]),
         PLOT_DISTANCE_INTERVAL,
     )
     interval_speeds = []
+    interval_hrs = []
     for interval in intervals:
         shortest_time = 9999
+        hr = 100
         for start_i, start_d in enumerate(run.distance):
             delta_i = None
             try:
@@ -90,16 +104,22 @@ for run in tqdm(runs):
             except StopIteration:
                 break
             else:
-                shortest_time = min(
-                    shortest_time,
-                    run.time[start_i + delta_i] - run.time[start_i],
-                )
+                interval_time = run.time[start_i + delta_i] - run.time[start_i]
+                if interval_time < shortest_time:
+                    shortest_time = interval_time
+                    if run.heartrate is not None:
+                        hr = sum(run.heartrate[start_i:][:delta_i]) / delta_i
         if shortest_time == 0:
             print(f"shortest_time is zero!?")
             interval_speeds.append(0)
         else:
             interval_speeds.append(interval / shortest_time)
-    plt.plot(intervals, interval_speeds)
+        interval_hrs.append(hr)
+    plt.plot(
+        intervals,
+        interval_speeds,
+        c=color_map(hr_to_01(interval_hrs)[0]),
+    )
 
 #plt.xscale("log")
 start, end = plt.gca().get_xlim()
