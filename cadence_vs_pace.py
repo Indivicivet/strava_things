68de68522cfa5744d7a05bd77f3bedf6c74529ab
indi <indivicivet@gmail.com>
2023-08-29 14:55:44 +0100

    add "plot stride length" option

--- cadence_vs_pace.py
+++ cadence_vs_pace.py
@@ -8,17 +8,25 @@ seaborn.set()
 
 runs = strava_shared.load_runs()
 
+PLOT_STRIDE_LENGTH = False
 
 def my_smooth(data, smooth_length=100):
-    return [
+    return np.array([
         np.mean(data[i:i+smooth_length])
         for i in range(len(data) - smooth_length)
-    ]
+    ])
 
 
 for run in runs:
-    plt.scatter(my_smooth(run.velocity), my_smooth(run.cadence), alpha=0.1, s=3)
+    smooth_vel = my_smooth(run.velocity)
+    smooth_cadence = my_smooth(run.cadence)
+    plt.scatter(
+        smooth_vel,
+        smooth_vel / (smooth_cadence * (2 / 60)) if PLOT_STRIDE_LENGTH else smooth_cadence,
+        alpha=0.1,
+        s=3,
+    )
 
 plt.xlabel("velocity")
-plt.ylabel("cadence")
+plt.ylabel("stride length" if PLOT_STRIDE_LENGTH else "cadence")
 plt.show()
