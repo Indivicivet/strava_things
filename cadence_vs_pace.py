93aa468e431205d665a1999372af2753064a004d
indi <indivicivet@gmail.com>
2023-08-29 14:44:33 +0100

    add cadence vs pace line plot

---
+++ cadence_vs_pace.py
@@ -0,0 +1,21 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+import strava_shared
+
+runs = strava_shared.load_runs()
+
+
+def my_smooth(data, smooth_length=20):
+    return [
+        np.mean(data[i:i+smooth_length])
+        for i in range(len(data) - smooth_length)
+    ]
+
+
+for run in runs:
+    plt.plot(my_smooth(run.velocity), my_smooth(run.cadence), alpha=0.1)
+
+plt.xlabel("velocity")
+plt.ylabel("cadence")
+plt.show()
