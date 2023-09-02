b04acba10fbbc084ce56bb27100e59f4009177ae
indi <indivicivet@gmail.com>
2023-09-02 23:06:11 +0100

    don't require cadences in places_ive_been

--- places_ive_been.py
+++ places_ive_been.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 
 import strava_shared
 
-runs = strava_shared.load_runs()
+runs = strava_shared.load_runs(require_cadences=False)
 
 for run in runs:
     plt.scatter(*np.array(run.latlng).T)
