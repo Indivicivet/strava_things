281ee988acaa9999c208cf39cc87717d6a2ca7c1
indi <indivicivet@gmail.com>
2023-09-28 00:08:35 +0100

    pull y vel plot out to a variable

--- cadence_vs_pace.py
+++ cadence_vs_pace.py
@@ -22,9 +22,14 @@ def my_smooth(data, smooth_length=30):
 for run in runs:
     smooth_vel = my_smooth(run.velocity)
     smooth_cadence = my_smooth(run.cadence)
+    plot_y_vals = (
+        smooth_vel / (smooth_cadence / 60)
+        if PLOT_STRIDE_LENGTH
+        else smooth_cadence
+    )
     plt.scatter(
         smooth_vel,
-        smooth_vel / (smooth_cadence / 60) if PLOT_STRIDE_LENGTH else smooth_cadence,
+        plot_y_vals,
         # color=matplotlib.cm.get_cmap("PiYG").reversed()(run.distance[-1] / 30000),
         alpha=0.1,
         s=3,
