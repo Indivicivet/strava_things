e69c6335fe3ec9c3fe48efd7a59eef20d3fec237
indi <indivicivet@gmail.com>
2024-02-12 14:39:28 +0000

    skip runs with no latlng (treadmill)

--- places_ive_been.py
+++ places_ive_been.py
@@ -6,6 +6,9 @@ import strava_shared
 runs = strava_shared.load_runs(require_cadences=False)
 
 for run in runs:
+    if not run.latlng:
+        print(f"run missing latlng {run}, ignoring")
+        continue
     plt.plot(
         *np.array(run.latlng).T[::-1],
         alpha=0.2,
