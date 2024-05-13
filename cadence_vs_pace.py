6313335bbc031f2b23a0a9b8b4bf91e85259d178
indi <indivicivet@gmail.com>
2024-05-13 23:21:30 +0100

    add HIGHLIGHT_RUN option to cadence_vs_pace

--- cadence_vs_pace.py
+++ cadence_vs_pace.py
@@ -10,6 +10,7 @@ seaborn.set()
 runs = strava_shared.load_runs()
 
 PLOT_STRIDE_LENGTH = False
+HIGHLIGHT_RUN = "latest"
 
 
 def my_smooth(data, smooth_length=30):
@@ -23,7 +24,7 @@ plot_runs = runs[:50]
 
 
 plt.figure(figsize=(12.8, 7.2))
-for run in plot_runs:
+for i, run in enumerate(plot_runs):
     smooth_vel = my_smooth(run.velocity)
     smooth_cadence = my_smooth(run.cadence)
     plot_y_vals = (
@@ -35,7 +36,8 @@ for run in plot_runs:
         smooth_vel,
         plot_y_vals,
         # color=matplotlib.cm.get_cmap("PiYG").reversed()(run.distance[-1] / 30000),
-        alpha=0.1,
+        color=("red" if i == 0 else "black") if HIGHLIGHT_RUN == "latest" else None,
+        alpha=(1 if i ==0 else 0.05) if HIGHLIGHT_RUN == "latest" else 0.1,
         s=3,
     )
     if len(plot_runs) <= 5:
