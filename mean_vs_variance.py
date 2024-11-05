d86e6214e68b0d641602af9a137d0f3336e9ff7a
indi <indivicivet@gmail.com>
2024-11-05 00:44:45 +0000

    add absolutely degenerate coloration of run values

--- mean_vs_variance.py
+++ mean_vs_variance.py
@@ -9,7 +9,11 @@ measure_vals = [run.velocity for run in runs]
 plt.scatter(
     [np.mean(seq) for seq in measure_vals],
     [np.var(seq) for seq in measure_vals],
+    c=[run.distance[-1] for run in runs],
+    alpha=np.clip([run.distance[-1]/10000 for run in runs], 0, 1),
+    s=[run.distance[-1]/100 for run in runs],
 )
 plt.xlabel("mean")
 plt.ylabel("variance")
+plt.colorbar()
 plt.show()
