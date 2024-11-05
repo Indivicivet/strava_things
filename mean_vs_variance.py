86ff54e1fcc2550ee4432c97be0c76458e77140a
indi <indivicivet@gmail.com>
2024-11-05 00:46:19 +0000

    clean up logic with nicer np

--- mean_vs_variance.py
+++ mean_vs_variance.py
@@ -6,12 +6,13 @@ import strava_shared
 runs = strava_shared.load_runs()
 
 measure_vals = [run.velocity for run in runs]
+dist_arr = np.array([run.distance[-1] for run in runs])
 plt.scatter(
     [np.mean(seq) for seq in measure_vals],
     [np.var(seq) for seq in measure_vals],
-    c=[run.distance[-1] for run in runs],
-    alpha=np.clip([run.distance[-1]/10000 for run in runs], 0, 1),
-    s=[run.distance[-1]/100 for run in runs],
+    c=dist_arr,
+    alpha=np.clip(dist_arr / 10000, 0, 1),
+    s=dist_arr / 100,
 )
 plt.xlabel("mean")
 plt.ylabel("variance")
