2282b25de28a318859008332c34241231510c942
indi <indivicivet@gmail.com>
2024-11-05 00:25:29 +0000

    kde for recent runs only

--- pace_kde.py
+++ pace_kde.py
@@ -7,7 +7,7 @@ import strava_shared
 runs = strava_shared.load_runs()
 
 seaborn.kdeplot(
-    [run.velocity for run in runs],
+    [run.velocity for run in runs[-10:]],
 )
 plt.xlabel("velocity")
 plt.show()
