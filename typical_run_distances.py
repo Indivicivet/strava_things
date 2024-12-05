785dda664ed0e41802eee3b944db5e1b2a21d078
indi <indivicivet@gmail.com>
2024-12-05 01:07:57 +0000

    most naive possible KDE estimator for run lengths

---
+++ typical_run_distances.py
@@ -0,0 +1,18 @@
+"""
+KDE for run distances
+"""
+
+from matplotlib import pyplot as plt
+import seaborn
+
+import strava_shared
+
+runs = strava_shared.load_runs()
+
+RECENT_N = 100
+
+plt.figure(figsize=(12.8, 7.2))
+seaborn.kdeplot([run.distance[-1] for run in runs])
+plt.xlabel("run length (m)")
+plt.show()
+
