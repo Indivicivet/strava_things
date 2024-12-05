cbbeadd92e19a80cc543ef2f00591423c09d5c5d
indi <indivicivet@gmail.com>
2024-12-05 01:13:08 +0000

    add smooth and less-smooth plot for run distances

--- typical_run_distances.py
+++ typical_run_distances.py
@@ -15,6 +15,14 @@ plt.figure(figsize=(12.8, 7.2))
 seaborn.kdeplot(
     [run.distance[-1] for run in runs],
     cut=0,
+    bw_adjust=0.15,
+    label="slightly smoothed",
+)
+seaborn.kdeplot(
+    [run.distance[-1] for run in runs],
+    cut=0,
+    bw_adjust=2,
+    label="smooth",
 )
 plt.xlabel("run length (m)")
 plt.show()
