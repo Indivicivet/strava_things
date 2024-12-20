c9797d3bc8ab44db5e4ac71552e822b3b4692b46
indi <indivicivet@gmail.com>
2024-12-20 22:34:23 +0000

    length weighted places I've been map - nicer! (except only works well for your primary location)

--- places_ive_been.py
+++ places_ive_been.py
@@ -5,12 +5,20 @@ import strava_shared
 
 runs = strava_shared.load_runs(require_cadences=False)
 
+LENGTH_WEIGHTING = 1.5
+
+max_len = max(run.distance[-1] for run in runs)
+
 for run in runs:
     if not run.latlng:
         print(f"run missing latlng {run}, ignoring")
         continue
     plt.plot(
         *np.array(run.latlng).T[::-1],
-        alpha=0.2,
+        alpha=(
+            (run.distance[-1] / max_len) ** LENGTH_WEIGHTING
+            if LENGTH_WEIGHTING > 0
+            else 0.2
+        ),
     )
 plt.show()
