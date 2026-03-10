ecb08f4f56da8c54cd26fbabb953e7c58da664b7
indi <indivicivet@gmail.com>
2026-03-10 17:54:43 +0000

    refactor strava load_activities() so that it calls out to separate functions for json vs gpx

--- strava_shared.py
+++ strava_shared.py
@@ -34,98 +34,105 @@ class Activity:
     activity_type: ActivityType = ActivityType("Run")
 
 
+
+def load_activity_json(path: Path, require_cadences: bool = True) -> Optional[Activity]:
+    info = json.loads(path.read_text())
+    this_velocities = None
+    this_distances = None
+    this_times = None
+    this_cadences = None
+    this_hrs = None
+    this_latlng = None
+    streams = info["streams"]
+    if isinstance(streams, list):
+        # old format, pre 2025-09ish: convert to dict layout
+        streams = {s["type"]: s for s in streams}
+
+    if isinstance(streams, dict):
+        # unified format (dict of dicts)
+        def _getdata(label):
+            stream = streams.get(label)
+            if stream is None:
+                return None
+            return stream["data"]
+
+        this_velocities = _getdata("velocity_smooth")
+        this_distances = _getdata("distance")
+        this_times = _getdata("time")
+        this_cadences = _getdata("cadence")
+        this_hrs = _getdata("heartrate")
+        this_latlng = _getdata("latlng")
+    else:
+        raise ValueError(f"unclear what is {streams=}, expected dict or list")
+    if (
+        this_distances is None
+        or this_velocities is None
+        or this_times is None
+        or (this_cadences is None and require_cadences)
+    ):
+        print(f"got an activity missing things, {path.stem=}, {require_cadences=}")
+        return None
+    return Activity(
+        activity_id=path.stem.split("_")[1],
+        velocity=this_velocities,
+        distance=this_distances,
+        time=this_times,
+        # cadences are in cycles/min, I want spm
+        cadence=(
+            [2 * c for c in this_cadences]
+            if this_cadences is not None
+            else None
+        ),
+        heartrate=this_hrs,
+        latlng=this_latlng,
+        date=datetime.datetime.strptime(
+            info["metadata"]["start_date"], "%Y-%m-%dT%H:%M:%SZ"
+        ).replace(tzinfo=datetime.timezone.utc),
+        # ^todo :: proper timezone handling?
+        activity_type=ActivityType(info["metadata"].get("type", "Run")),
+    )
+
+
+def load_activity_gpx(path: Path) -> Activity:
+    with open(path) as f:
+        gpx = gpxpy.parse(f)
+    if len(gpx.tracks) > 1:
+        print(f"{len(gpx.tracks)=} > 1, only using first")
+    track = gpx.tracks[0]
+    if len(track.segments) > 1:
+        print(f"{len(track.segments)=} > 1, only using first")
+    segment = track.segments[0]
+    distances = [0]
+    for pt1, pt2 in zip(segment.points, segment.points[1:]):
+        distances.append(distances[-1] + pt1.distance_3d(pt2))
+    return Activity(
+        activity_id=path.stem.split("_")[1],
+        velocity=None,  # todo
+        distance=distances,
+        time=[point.time for point in segment.points],
+        # cadences are in cycles/min, I want spm
+        cadence=[
+            2 * int(point.extensions[0].getchildren()[1].text)
+            for point in segment.points
+        ],
+        # todo :: these could probs do with being more robust
+        heartrate=[
+            int(point.extensions[0].getchildren()[0].text)
+            for point in segment.points
+        ],
+        latlng=[(point.latitude, point.longitude) for point in segment.points],
+        date=gpx.time,
+        # GPX doesn't easily store type in a standard way here:
+        activity_type=ActivityType("Run"),
+    )
+
+
 def load_activities(require_cadences=True):
     activities = []
     for p in MY_DATA_FOLDER.glob("*.json"):
-        info = json.loads(p.read_text())
-        this_velocities = None
-        this_distances = None
-        this_times = None
-        this_cadences = None
-        this_hrs = None
-        this_latlng = None
-        streams = info["streams"]
-        if isinstance(streams, list):
-            # old format, pre 2025-09ish: convert to dict layout
-            streams = {s["type"]: s for s in streams}
-
-        if isinstance(streams, dict):
-            # unified format (dict of dicts)
-            def _getdata(label):
-                stream = streams.get(label)
-                if stream is None:
-                    return None
-                return stream["data"]
-
-            this_velocities = _getdata("velocity_smooth")
-            this_distances = _getdata("distance")
-            this_times = _getdata("time")
-            this_cadences = _getdata("cadence")
-            this_hrs = _getdata("heartrate")
-            this_latlng = _getdata("latlng")
-        else:
-            raise ValueError(f"unclear what is {streams=}, expected dict or list")
-        if (
-            this_distances is None
-            or this_velocities is None
-            or this_times is None
-            or (this_cadences is None and require_cadences)
-        ):
-            print(f"got an activity missing things, {p.stem=}, {require_cadences=}")
-            continue
-        activities.append(
-            Activity(
-                activity_id=p.stem.split("_")[1],
-                velocity=this_velocities,
-                distance=this_distances,
-                time=this_times,
-                # cadences are in cycles/min, I want spm
-                cadence=(
-                    [2 * c for c in this_cadences]
-                    if this_cadences is not None
-                    else None
-                ),
-                heartrate=this_hrs,
-                latlng=this_latlng,
-                date=datetime.datetime.strptime(
-                    info["metadata"]["start_date"], "%Y-%m-%dT%H:%M:%SZ"
-                ).replace(tzinfo=datetime.timezone.utc),
-                # ^todo :: proper timezone handling?
-                activity_type=ActivityType(info["metadata"].get("type", "Run")),
-            )
-        )
+        activity = load_activity_json(p, require_cadences=require_cadences)
+        if activity is not None:
+            activities.append(activity)
     for p in MY_DATA_FOLDER.glob("*.gpx"):
-        with open(p) as f:
-            gpx = gpxpy.parse(f)
-        if len(gpx.tracks) > 1:
-            print(f"{len(gpx.tracks)=} > 1, only using first")
-        track = gpx.tracks[0]
-        if len(track.segments) > 1:
-            print(f"{len(track.segments)=} > 1, only using first")
-        segment = track.segments[0]
-        distances = [0]
-        for pt1, pt2 in zip(segment.points, segment.points[1:]):
-            distances.append(distances[-1] + pt1.distance_3d(pt2))
-        activities.append(
-            Activity(
-                activity_id=p.stem.split("_")[1],
-                velocity=None,  # todo
-                distance=distances,
-                time=[point.time for point in segment.points],
-                # cadences are in cycles/min, I want spm
-                cadence=[
-                    2 * int(point.extensions[0].getchildren()[1].text)
-                    for point in segment.points
-                ],
-                # todo :: these could probs do with being more robust
-                heartrate=[
-                    int(point.extensions[0].getchildren()[0].text)
-                    for point in segment.points
-                ],
-                latlng=[(point.latitude, point.longitude) for point in segment.points],
-                date=gpx.time,
-                # GPX doesn't easily store type in a standard way here:
-                activity_type=ActivityType("Run"),
-            ),
-        )
+        activities.append(load_activity_gpx(p))
     return sorted(activities, key=lambda r: r.date, reverse=True)
